5e64e17bf363392f5630ea3aad153deac426dc59
Igor Kuzmin <lamnya@gmail.com>
2021-12-07 18:15:39 +0800

    Initial commit

---
+++ DZ10/DZ10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ10
+//
+//  Created by Igor Kuzmin on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
